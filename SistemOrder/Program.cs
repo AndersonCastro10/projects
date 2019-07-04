08a6a51a221877ac6df07ed798e1502ccab77605
Anderson Castro <anderson-lima-10@hotmail.com>
2019-07-03 22:03:58 -0300

    Projeto iniciado, mostrando a msg 'ola mundo'

---
+++ SistemOrder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SistemOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
